765b70e12774cf988cb52ecff25febed81d308d9
Stef Siekman <s.siekman@gmail.com>
2018-06-06 00:07:04 +0200

    Fix typo is variable

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (!fs.existsSync(options.project)) {
     process.exit(1)
 }
 
-if (option.generate && !fs.existsSync(options.wiki)) {
+if (options.generate && !fs.existsSync(options.wiki)) {
     console.error('Invalid wiki location')
     process.exit(1)
 }
